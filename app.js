bdfc11aefc179f48996ad6f02e82809009d6ce8d
Baron <a0122545@u.nus.edu>
2016-10-09 07:13:16 +0800

    Removed stdin console in attempt to get heroku working

--- app.js
+++ app.js
@@ -37,25 +37,25 @@ module.exports = app;
 
 
 
-var stdin = process.stdin;
+// var stdin = process.stdin;
 
-// without this, we would only get streams once enter is pressed
-stdin.setRawMode(true);
+// // without this, we would only get streams once enter is pressed
+// stdin.setRawMode(true);
 
-// resume stdin in the parent process (node app won't quit all by itself
-// unless an error or process.exit() happens)
-stdin.resume();
+// // resume stdin in the parent process (node app won't quit all by itself
+// // unless an error or process.exit() happens)
+// stdin.resume();
 
-// i don't want binary, do you?
-stdin.setEncoding('utf8');
+// // i don't want binary, do you?
+// stdin.setEncoding('utf8');
 
-// on any data into stdin
-stdin.on('data', function (key) {
-  // ctrl-c ( end of text )
-  if (key === '\u0003') {
-    process.exit();
-  }
+// // on any data into stdin
+// stdin.on('data', function (key) {
+//   // ctrl-c ( end of text )
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
 
-  console.log("Queue moved");
-  queueManager.dequeueTicket();
-});
+//   console.log("Queue moved");
+//   queueManager.dequeueTicket();
+// });
